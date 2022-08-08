d742aeab1b7a8d0eded230e217baac412c44e1b9
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-07 17:00:12 -0700

    Create a Model

--- myApp.js
+++ myApp.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
-  }
+  },
+  age: Number,
+  favoriteFoods: [String]
 })
 
 const Person = mongoose.model('Person', personSchema);
-
-let person1 = new Person();
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
